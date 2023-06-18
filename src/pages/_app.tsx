02ed17e9cd66559d0128390e16b3ee8da2346b18
shantanujumde <jumde21@gmail.com>
2023-06-18 17:06:44 +0530

    changed schema, updated app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "@components/utils/api";
 import "@components/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,7 +11,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Next Gen Blog </title>
+        <meta name="description" content="this is a blog site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex">
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
